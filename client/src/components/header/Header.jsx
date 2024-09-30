eb87d4355f02943dc740dc42b8e57f7c5317977a
dishant <aryadishant414@gmail.com>
2024-09-30 08:00:45 +0530

    Starting new Mern Project

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import {AppBar, Toolbar, styled } from '@mui/material';
+
+
+
+
+const StyledHeader = styled(AppBar)`
+  background: #2874f0;
+  height: 55px;
+
+`;
+
+
+const Header = () => {
+    return (
+        <>
+            <StyledHeader>
+                <Toolbar>
+                
+                </Toolbar>
+            </StyledHeader>
+        </>
+    )
+};
+
+export default Header;
