92980de59fdeab997ac7646185483e36c32efc9d
dishant <aryadishant414@gmail.com>
2024-10-01 09:02:39 +0530

    Completed Banner Carousel in home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,23 @@
 //components
 import NavBar from "./NavBar.jsx";
+import Banner from "./Banner.jsx";
+import { Box, styled } from "@mui/material";
 
+
+const Component = styled(Box)`
+    /* border: 2px solid red; */
+    padding: 10px;
+    background: #F2F2F2;
+`;
 
 const Home = () => {
     return (
         <>
             <NavBar />
+ 
+            <Component>
+                <Banner />
+            </Component>
         </>
     )
 };
